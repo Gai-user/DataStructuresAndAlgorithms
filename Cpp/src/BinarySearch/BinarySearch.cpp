d70b6a921bc05441ae73fbcfedd22d835ebd41fe
Gai <Gai@example.com>
2025-12-09 21:37:29 +0800

    feat: add binary search variant to find the first element equal to a given value

--- Cpp/src/BinarySearch/BinarySearch.cpp
+++ Cpp/src/BinarySearch/BinarySearch.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 template <typename T>
 int binary_search(std::vector<T>& array, T target)
@@ -7,10 +8,10 @@ int binary_search(std::vector<T>& array, T target)
     size_t left = 0;
     size_t right = array.size() - 1;
 
-    size_t mid = left + (right - left) / 2;
 
     while (left <= right)
     {
+        size_t mid = left + (right - left) / 2;
         T value = array[mid];
         if (value < target) {
             left = mid + 1;
@@ -44,6 +45,33 @@ int binary_search_recursive(std::vector<T>& array, T target, int left, int right
     }
 }
 
+// 查找第一个值等于给定值的元素下标
+template <typename T>
+int binary_search_first_value(std::vector<T> array, T target)
+{
+    int left = 0;
+    int right = array.size() - 1;
+
+    
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        T value = array[mid];
+        if (value > target) {
+            right = mid - 1;
+        } else if (value < target) {
+            left = mid + 1;
+        } else {
+            if (mid == 0 || array[mid - 1] != target) {
+                return mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
 
 int main() {
 
@@ -59,6 +87,10 @@ int main() {
     auto result3 = binary_search_recursive(array, 5.4, 0, array.size()-1);
 
     std::cout << "The value of result3 is " << result3 << std::endl;
+
+    std::vector<int> array1 = {1, 3, 4, 5, 6, 8, 8, 8, 11, 18};
+    auto result4 = binary_search_first_value(array1, 8);
+    assert(result4 == 5);
 
     return 0;
 }

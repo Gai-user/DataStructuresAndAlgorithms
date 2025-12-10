766162319f33fe9a0455e97973f29c07a5584b1b
Gai <Gai@example.com>
2025-12-10 21:04:48 +0800

    feat: add more variants bianry search

--- Cpp/src/BinarySearch/BinarySearch.cpp
+++ Cpp/src/BinarySearch/BinarySearch.cpp
@@ -47,7 +47,7 @@ int binary_search_recursive(std::vector<T>& array, T target, int left, int right
 
 // 查找第一个值等于给定值的元素下标
 template <typename T>
-int binary_search_first_value(std::vector<T> array, T target)
+int binary_search_first_value(std::vector<T>& array, T target)
 {
     int left = 0;
     int right = array.size() - 1;
@@ -73,6 +73,59 @@ int binary_search_first_value(std::vector<T> array, T target)
     return -1;
 }
 
+// 查找最后一个值等于给定值的元素
+template <typename T>
+int binary_search_end_value(std::vector<T>& array, T target)
+{
+    int left = 0;
+    int right = array.size() - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        T value = array[mid];
+        if (value < target)
+        {
+            left = mid + 1;
+        } else if (value > target) {
+            right = mid - 1;
+        } else {
+            if (mid == array.size() -1 || array[mid+1] != target)
+            {
+                return mid;
+            }
+            left = mid + 1;
+            
+        }
+        
+    }
+    return -1;
+}
+
+// 查找第一个大于等于给定值的元素
+template <typename T>
+int binary_search_first_greater(std::vector<T>& array, T target)
+{
+    int left = 0;
+    int right = array.size() - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        T value = array[mid];
+        if (value < target)
+        {
+            left = mid + 1;
+        } else {
+            if (mid == 0 || array[mid - 1] < target) {
+                return mid;
+            }
+            right = mid + 1;
+        }
+    }
+    return -1;
+}
+
 int main() {
 
     std::vector<double> array = {1.1, 3.4, 4.3, 4.5, 5.6, 7.0};
@@ -91,6 +144,13 @@ int main() {
     std::vector<int> array1 = {1, 3, 4, 5, 6, 8, 8, 8, 11, 18};
     auto result4 = binary_search_first_value(array1, 8);
     assert(result4 == 5);
+
+    auto result5 = binary_search_end_value(array1, 8);
+    assert(result5 == 7);
+    
+    std::vector<int> array2 = {1, 3, 4, 6, 7, 8, 9, 10};
+    auto result6 = binary_search_first_greater(array2, 5);
+    assert(result6 == 3);
 
     return 0;
 }

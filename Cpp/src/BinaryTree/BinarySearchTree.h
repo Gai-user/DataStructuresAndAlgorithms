4dec4ecaa0a0d5a36e6c0026fe168d7813618cc0
Gai <Gai@example.com>
2025-12-31 21:57:48 +0800

    feat: implement deletion a node in binary search tree
    the unit test case still needs to be added

--- Cpp/src/BinaryTree/BinarySearchTree.h
+++ Cpp/src/BinaryTree/BinarySearchTree.h
@@ -20,6 +20,7 @@ public:
     BinarySearchTree() = default;
     void insert(T value);
     TreeNode<T>* find(const T& value);
+    void remove(const T& data); // 只适用于没有重复值的二叉搜索树
 
 };
 
@@ -61,5 +62,62 @@ template <typename T>
 TreeNode<T>* BinarySearchTree<T>::find(const T& value)
 {
     return findHelper(this->node, value);
+}
+
+template <typename T>
+void BinarySearchTree<T>::remove(const T& data)
+{
+    if (this->node == nullptr) {
+        return;
+    }
+    TreeNode<T>* DNode = this->node.get(); // 需要删除的节点
+    TreeNode<T>* FNode = nullptr; // 删除节点的父节点
+
+    // 找到需要删除的节点
+    while (DNode != nullptr && DNode->value != data) {
+        FNode = DNode;
+        if (DNode->value > data) {
+            DNode = DNode->left.get();
+        } else {
+            DNode = DNode->right.get();
+        }
+    }
+    if (!DNode) {
+        return;
+    }
+
+    if (DNode->left && DNode->right) {
+        // 寻找右子树的最小节点
+        TreeNode<T>* minNode = DNode;
+        TreeNode<T>* FminNode = nullptr;
+        while (minNode->left)
+        {
+            FminNode = minNode;
+            minNode = minNode->left.get();
+        }
+        // 将当前删除节点替换为minNode
+        DNode->value = minNode->value;
+        // 现在需要删除的节点为右子树的最小节点
+        DNode = minNode;
+        FNode = FminNode;
+        
+    }
+
+    std::unique_ptr<TreeNode<T>> NextNode;
+    if (DNode->left) {
+        NextNode = std::move(DNode->left);
+    } else if (DNode->right) {
+        NextNode = std::move(DNode->right);
+    } 
+    if (!FNode) {
+        this->node = std::move(NextNode);
+        return;
+    }
+    if (FNode->left.get() == DNode) {
+        FNode->left = std::move(NextNode);
+    } else {
+        FNode->right = std::move(NextNode);
+    }
+
 }
 #endif // BIANRY_SEARCH_TREE_H

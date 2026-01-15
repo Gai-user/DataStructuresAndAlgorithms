03070c44d3f1a5518e455227a968578a72978ace
Gai <Gai@example.com>
2026-01-15 21:57:29 +0800

    feat: implement word search (LeetCode 79)

--- Cpp/src/Graph/Graph.h
+++ Cpp/src/Graph/Graph.h
@@ -3,6 +3,7 @@
 #define GRAPH_H
 
 #include <vector>
+#include <string>
 
 class Graph {
 public:
@@ -11,6 +12,7 @@ public:
     void AddEdge(int s, int t);
     std::vector<int> BreadthFirstSearch(int s, int t);
     std::vector<int> DepthFirstSearch(int s, int t);
+    static bool ExistWord(const std::vector<std::vector<char>>& board, const std::string& word);
     
 
 private:
@@ -18,6 +20,7 @@ private:
     std::vector<std::vector<int>> adj;
     std::vector<int> GetOrder(const std::vector<int>& q, int t);
     void DFSHelper(int s, int t, std::vector<bool>& visited, std::vector<int>& prev);
+    static bool CheckSubWord(const std::vector<std::vector<char>>& board, const std::string& word, std::vector<std::vector<bool>>& visited, int i, int j, int k);
 };
 
 #endif // GRAPH_H

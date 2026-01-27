d0579dd914747ebf000d98d8cc9e2d9c97c3d0a7
Gai <Gai@example.com>
2026-01-27 13:01:48 +0000

    feat(string): add Levenshtein distance backtracking
    Implement backtracking to reconstruct edit operations
    used in Levenshtein distance calculation.

--- Cpp/src/BackTrack/BackTrack.h
+++ Cpp/src/BackTrack/BackTrack.h
@@ -2,6 +2,8 @@
 #define BACK_TRACK_H
 
 #include <vector>
+#include <string>
+#include <limits>
 
 class BackTrack {
 public:
@@ -10,9 +12,14 @@ public:
 
     void EightQueens(int row, std::vector<std::vector<int>>& result);
 
+    int LevenshteinDistance(const std::string& word1, const std::string& word2);
+
 private:
+    void LevenshteinDistanceHelper(const std::string& word1, const std::string& word2, int i, int j, int count);
     std::vector<int> position = std::vector<int>(8); // 记录棋盘布局
     bool CanPlaceQueen(int row, int col);
+    int MaxValue = std::numeric_limits<int>::max();
+
 };
 
 #endif // BACK_TRACK_H
